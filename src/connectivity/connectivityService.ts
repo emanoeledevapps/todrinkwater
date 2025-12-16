a9160f90f13b9034002cecafd3b3af3c8d5c4387
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-12-16 17:36:38 -0300

    version 1.1.3

--- src/connectivity/connectivityService.ts
+++ src/connectivity/connectivityService.ts
@@ -1,4 +1,4 @@
-import { Platform } from "react-native";
+import { PermissionsAndroid, Platform } from "react-native";
 import { sendMessage } from "react-native-wear-connectivity";
 import { format } from "date-fns";
 
@@ -12,6 +12,13 @@ interface SendListDayProps {
 }
 async function sendListDay({ date, origin }: SendListDayProps): Promise<void> {
   if (Platform.OS === "ios") return;
+
+  const permissions = await PermissionsAndroid.requestMultiple([
+    PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+    PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+  ]);
+  if (permissions["android.permission.BLUETOOTH_CONNECT"] !== "granted") return;
+  if (permissions["android.permission.BLUETOOTH_SCAN"] !== "granted") return;
 
   const list = await dbService.getConsumptionPerDay({ formattedDate: format(date, "dd/MM/yyyy") })
   const msg: MessageListDayProps = {
@@ -30,8 +37,15 @@ async function sendListDay({ date, origin }: SendListDayProps): Promise<void> {
 interface GetListDayProps {
   origin: MessageOrigin;
 }
-function getListDay({ origin }: GetListDayProps): void {
+async function getListDay({ origin }: GetListDayProps): Promise<void> {
   if (Platform.OS === "ios") return;
+
+  const permissions = await PermissionsAndroid.requestMultiple([
+    PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+    PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+  ]);
+  if (permissions["android.permission.BLUETOOTH_CONNECT"] !== "granted") return
+  if (permissions["android.permission.BLUETOOTH_SCAN"] !== "granted") return
 
   const msg: MessageGetListDay = {
     messageOrigin: origin,
